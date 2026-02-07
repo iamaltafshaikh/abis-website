75012c614e4d3d7a78815dd3ec38176e868c4824
iamaltafshaikh <altaf.shaikh.cse@gmail.com>
2026-02-07 13:33:38 +0530

    Updated home section, dark theme and about section layout

--- script.js
+++ script.js
@@ -29,3 +29,32 @@ function reveal() {
 }
 
 reveal();
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  dots.forEach(dot => dot.classList.remove("active"));
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+
+  currentSlide = index;
+}
+
+function nextSlide() {
+  let next = (currentSlide + 1) % slides.length;
+  showSlide(next);
+}
+
+setInterval(nextSlide, 5000);
+
+// Dot click control
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    showSlide(index);
+  });
+});
+
